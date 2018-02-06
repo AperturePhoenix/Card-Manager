f4ae94ab83900960d93b7e2f6aaf03efbc892e47
AperturePhoenix <aperture.phoenix@gmail.com>
2018-02-05 23:11:07 -0600

    set ListView to fill parent

--- src/controller/CardController.java
+++ src/controller/CardController.java
@@ -1,5 +1,9 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -7,9 +11,10 @@ import java.util.ResourceBundle;
  * Created by Lance Judan on 1/25/2018
  */
 public class CardController extends Controller {
+    @FXML ListView cardListView;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        cardListView.setItems(FXCollections.observableArrayList("Test", "Test2", "Test3"));
     }
 }
--- src/core/Main.java
+++ src/core/Main.java
@@ -1,17 +1,13 @@
 package core;
 
+import controller.Controller;
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.control.*;
-import javafx.scene.layout.GridPane;
+import javafx.scene.Scene;
+import javafx.scene.control.SplitPane;
 import javafx.stage.Stage;
 import managers.CardManager;
-
-import java.util.Optional;
 
 /**
  * Created by Lance Judan on 1/21/2018
@@ -19,15 +15,26 @@ import java.util.Optional;
 public class Main extends Application {
     private CardManager cardManager;
 
+    //Window variables
+    private static final String TITLE = "Card Manager";
+    private static final int PREFERED_WIDTH = 800, PREFERED_HEIGHT = 550;
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception {
         cardManager = new CardManager();
 
         FXMLLoader cardLoader = new FXMLLoader(getClass().getResource("/views/CardView.fxml"));
+        cardLoader.setRoot(new SplitPane());
+        Parent root = cardLoader.load();
+        ((Controller) cardLoader.getController()).setCardManager(cardManager);
+
+        primaryStage.setTitle(TITLE);
+        primaryStage.setScene(new Scene(root, PREFERED_WIDTH, PREFERED_HEIGHT));
+        primaryStage.show();
     }
 
     @Override
